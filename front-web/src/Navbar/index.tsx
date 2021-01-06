f8d4ac9a3a1cafbe3167c988e4b1ece6faa3e310
Vilson Moro <vilsonmoro@gmail.com>
2021-01-06 16:26:17 -0300

    Add Home

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,9 +1,9 @@
 import "./style.css";
-import { ReactComponent as Logo} from "./logo.svg";
+import { ReactComponent as Logo } from "./logo.svg";
 function Navbar(){
     return(
         <nav className="main-navbar">
-           
+           <Logo/>
            <a href='home' className="logo-text">DS Delivery</a>
         </nav>
     )
