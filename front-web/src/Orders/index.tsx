c1e52624e98fb22e25d3dfe1c4b8c1ae0cb1c5ae
Vilson Moro <vilsonmoro@gmail.com>
2021-01-07 12:19:43 -0300

    Consulta produtos

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../api";
+import ProductList from "./ProductList";
 import StepsHeader from "./StepsHeader";
 import "./styles.css";
+import { Product } from "./types";
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products)
+    useEffect( () => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
 
     return(
         <div className="orders-container">
-            <StepsHeader />            
+            <StepsHeader />     
+            <ProductList products={products}/>       
         </div>
     )
 }
